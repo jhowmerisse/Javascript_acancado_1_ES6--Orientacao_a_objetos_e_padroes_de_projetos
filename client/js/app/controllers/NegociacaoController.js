76c1e3ef7f54851db016972565463c7ce0f6d605
Jhovani Cesar Merisse <Jhovani.Merisse@MIDGARD.LOCAL>
2018-07-26 13:58:05 -0300

    implementacao spread operator, arrow function e map em negociacaoController

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,6 +1,7 @@
 class NegociacaoController {
 
   constructor() {
+    //bind faz com que a referencia entre querySelector e document nao seja perdida.
     let $ = document.querySelector.bind(document);
 
     this._inputData = $('#data');
@@ -11,8 +12,35 @@ class NegociacaoController {
   adiciona(event) {
     event.preventDefault();
 
-      let data= new Date(this._inputData.value.replace(/-/g, /,/));
-      console.log('data :', data);
+    
+    // let data= new Date(this._inputData.value.replace(/-/g, /,/));
+
+
+    /** uso de funcao anonima e spread operator (...) onde cada elemento se torna um parametro passado*/
+    /** split trasnforma string em um array separando por - */
+    /**map retorna novo array e executa algo em cada item do array */
+    // let data = new Date(...this._inputData.value
+    
+    //   .split(/-/)
+    //   .map(function(item, indice){
+    //     return item - indice % 2;
+    // }));
+
+
+    /**arrow function (=>) onde ja é feito o retorno automaticamente sem o return*/
+    let data = new Date(
+      ...this._inputData.value
+        .split('-')
+        .map((item, indice) => item - indice % 2)
+    );
+
+    let negociacao = new Negociacao(
+      data,
+      this._inputQuantidade.value,
+      this._inputValor.value
+    );
+    console.log(negociacao);
+
 
   }
 }
