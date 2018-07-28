af26b6c3cfe3117f3da73fa9afaa1f22bd69aae2
jhow <jhow.merisse@hotmail.com>
2018-07-28 16:38:04 -0300

    implementação de DateHelper e ListaNegociacoes ao controller

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,46 +1,39 @@
 class NegociacaoController {
 
   constructor() {
-    //bind faz com que a referencia entre querySelector e document nao seja perdida.
+
     let $ = document.querySelector.bind(document);
 
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
+    this._ListaNegociacoes = new ListaNegociacoes();
   }
 
   adiciona(event) {
+
     event.preventDefault();
 
-    
-    // let data= new Date(this._inputData.value.replace(/-/g, /,/));
+    this._ListaNegociacoes.adiciona(this._criaNegociacao());
 
+    this._limpaFormulario();
+  }
 
-    /** uso de funcao anonima e spread operator (...) onde cada elemento se torna um parametro passado*/
-    /** split trasnforma string em um array separando por - */
-    /**map retorna novo array e executa algo em cada item do array */
-    // let data = new Date(...this._inputData.value
-    
-    //   .split(/-/)
-    //   .map(function(item, indice){
-    //     return item - indice % 2;
-    // }));
-
-
-    /**arrow function (=>) onde ja é feito o retorno automaticamente sem o return*/
-    let data = new Date(
-      ...this._inputData.value
-        .split('-')
-        .map((item, indice) => item - indice % 2)
-    );
-
-    let negociacao = new Negociacao(
-      data,
+  _criaNegociacao() {
+    return new Negociacao(
+      DateHelper.textoParaData(this._inputData.value),
       this._inputQuantidade.value,
       this._inputValor.value
     );
-    console.log(negociacao);
-
-
   }
+
+  _limpaFormulario() {
+
+    this._inputData.value = '';
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0.0
+
+    this._inputData.focus();
+  }
+
 }
