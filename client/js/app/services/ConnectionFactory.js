e2cf2bc0b25d1a19e5b996a1d91ef86db3b0f07f
jhowmerisse <jhow.merisse@hotmail.com>
2018-12-09 14:14:44 -0200

    implementando Module Pattern e Monkey Patch na ConnectionFactory

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,32 +1,75 @@
-var stores = ['negociacoes'];
-var version = 1;
-var dbName = 'banco';
+//Module Pattern
+//função autoinvocada
+var ConnectionFactory = (function () {
 
-class ConnectionFactory {
+    const stores = ['negociacoes'];
+    const version = 1;
+    const dbName = 'banco';
+    
+    var connection = null;
 
-    constructor() {
+    var close = null;
+    
+    //exporta a classe através do módulo e mantém a variáveis protegidas
+    return class ConnectionFactory {
+    
+        constructor() {
+    
+            throw new Error('Não é possível criar instâncias de ConnectionFactory');
+        }
+    
+        static getConnection() {
+    
+            return new Promise((resolve, reject) => {
+    
+                let openRequest = window.indexedDB.open(dbName, version)
+    
+                openRequest.onupgradeneeded = e => {
+    
+                    ConnectionFactory._createStores(e.target.result);
+                };
+                
+                openRequest.onsuccess = e => {
+                    
+                    //Monkey Patch -- modifica a api a força
+                    //ignora o código anterior e executa o código criado
+                    if(!connection){
+                        connection = e.target.result;
+                        close = connection.close.bind(connection);
+                        connection.close = function() {
+                            throw new Error('Você não pode fechar diretamente a conexão');
+                        };
+                    }
+    
+                    resolve(e.target.result);
+                };
+                
+                openRequest.onerror = e => {
+                    
+                    console.log(e.target.error);
+    
+                    reject(e.target.error.name);
+                };
+            });
+        }
+        
+        static _createStores(connection) {
+            
+            stores.forEach(store => {
+    
+                if (connection.objectStoreNames.contains(store))
+                    connection.deleteObjectStore(store);
+    
+                connection.createObjectStore(store, { autoIncrement: true});
+            });
+        }
 
-        throw new Error('Não é possível criar instâncias de ConnectionFactory');
+        static closeConnection() {
+
+            if(connection) {
+                close();
+                connection = null;
+            }
+        }
     }
-
-    static getConnection() {
-
-        return new Promise((resolve, reject) => {
-
-            let openRequest = window.indexedDB.open(dbName, version)
-
-            openRequest.onupgradeneeded = e => {
-
-
-            };
-
-            openRequest.onsuccess = e => {
-
-            };
-
-            openRequest.onerror = e => {
-
-            };
-        });
-    }
-}
+})();
